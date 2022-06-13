4adf3d60f2d956d52616179e81097087ec557484
Matthew Rhoads <mematthew123@gmail.com>
2022-06-12 20:47:15 -0600

    updated ui

--- mammoth-disc-golf/schemas/team.js
+++ mammoth-disc-golf/schemas/team.js
@@ -1,20 +1,11 @@
 export default {
-  name: "team",
-  type: "document",
-  title: "Team",
+  name: 'team',
+  type: 'document',
+  title: 'Team',
   fields: [
     {
-      name: "title",
-      type: "string",
-    },
-    {
-      name: "date",
-      type: "datetime",
-    },
-    {
-      name: "place",
-      title: "Home Town",
-      type: "string",
+      name: 'title',
+      type: 'string',
     },
 
     {
@@ -27,43 +18,23 @@ export default {
       },
     },
     {
-      name: "description",
-      title: "Bio",
-      type: "text",
+      name: 'description',
+      title: 'Bio',
+      type: 'text',
     },
 
-
-
     {
-      name: "mainImage",
-      title: "Main image",
-      type: "image",
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
       options: {
         hotspot: true,
       },
-    
-    },
-
-
-
-    {
-      name: "disc",
-      type: "string",
-      title: "Disc",
-      options: {
-        list: [
-          { value: "boss", title: "Boss" },
-          { value: "buzz", title: "Buzz" },
-          { value: "esp", title: "ESP Putter" },
-        ],
-      },
     },
 
     {
-      name: "link",
-      type: "url",
+      name: 'link',
+      type: 'url',
     },
-
-    
   ],
-};
+}
